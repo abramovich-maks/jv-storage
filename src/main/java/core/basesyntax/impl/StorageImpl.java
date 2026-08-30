67c4a736a6482175abbe1256be89e7345f35edc0
Maksim Abramovich <maks.abramovich.1995@mail.ru>
2026-08-30 19:30:31 +0200

    Implement `StorageImpl` with basic functionality: `put`, `get`, and `size`.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,45 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    public static final int MAX_STORAGE_SIZE = 10;
+    private K[] keys;
+    private V[] values;
+    private int size = 0;
+    private int index;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_STORAGE_SIZE];
+        values = (V[]) new Object[MAX_STORAGE_SIZE];
+    }
+
     @Override
     public void put(K key, V value) {
+        if (get(key) != null) {
+            for (int i = 0; i < size; i++) {
+                if (key == keys[i] || (key != null && key.equals(keys[i]))) {
+                    values[i] = value;
+                    return;
+                }
+            }
+        }
+        keys[index] = key;
+        values[index] = value;
+        index++;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key == keys[i] || (key != null && key.equals(keys[i]))) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
